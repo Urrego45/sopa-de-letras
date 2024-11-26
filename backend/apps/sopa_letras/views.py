4667eaa8436330e092af954f774d65544eb169f7
adhaliel <santiagourregou.4@gmail.com>
2024-11-26 11:15:37 -0500

    ajustes

--- backend/apps/sopa_letras/views.py
+++ backend/apps/sopa_letras/views.py
@@ -6,7 +6,9 @@ from rest_framework import status
 from rest_framework.exceptions import ValidationError
 import re
 
-import pprint
+_WORD_SEARCH_LEN = 196
+_MAX_ROW_LEN = 13
+_MIN_ROW_LEN = 0
 
 class pruebaApi(APIView):    
     def post (self, request):
@@ -15,21 +17,21 @@ class pruebaApi(APIView):
 
         word_search = [letters[i:i+14] for i in range(0, len(letters), 14)]
 
-        result = self.send_words(word_search, words)
+        result = self.search_words(word_search, words)
 
         return Response(result, status=status.HTTP_200_OK)
 
     def format_letter_words(self, letter, words):
 
         letters_array = list(re.sub(r'[^a-zA-Z]', '', letter).upper())
-        words_array = [word.lower() for word in re.findall(r'\b[a-zA-Z]+\b', words)]
+        words_array = [word.upper() for word in re.findall(r'\b[a-zA-Z]+\b', words)]
 
-        if len(letters_array) is not 196:
+        if len(letters_array) != _WORD_SEARCH_LEN:
             raise ValidationError({'message': f'la cantidad de letras debe ser igual a 196 caracteres. letras actuales: {len(letters_array)}'})
 
         return letters_array, words_array
     
-    def send_words(self, word_search, words):
+    def search_words(self, word_search, words):
 
         result = []
 
@@ -37,46 +39,43 @@ class pruebaApi(APIView):
 
         for word in words:
 
-            upper_word = word.upper()
-
             for row, index in enumerate(word_search):
 
-                if upper_word[0] in index:
+                if word[0] in index:
 
-                    position = [i for i, value in enumerate(index) if value == upper_word[0]]
+                    position = [i for i, value in enumerate(index) if value == word[0]]
 
-                    message = self.send(row, position, upper_word, index, word_search)
+                    message = self.search(row, position, word, index, word_search)
 
                     if message:
                         result.append(message)
                         break
             
             if not message:
-                result.append(f'La palabra {upper_word} no fue encontrada')
+                result.append(f'La palabra {word} no fue encontrada')
 
         return result
     
-    def send(self, row, position, word, index, word_search):
+    def search(self, row, position, word, index, word_search):
 
         message = ''
 
-        message = self.send_horizontal(index, word, row)
+        message = self.search_horizontal(index, word, row)
 
         if message: 
             return message
 
-        message = self.send_vertical(index, word, row, position, word_search)
+        message = self.search_vertical(word, row, position, word_search)
 
         if message:
             return message
 
-        message = self.send_diagonal(index, word, row, position, word_search)
+        message = self.send_diagonal(word, row, position, word_search)
 
         if message:
             return message
 
-
-    def send_horizontal(self, index, word, row):
+    def search_horizontal(self, index, word, row):
 
         msg = 'la palabra {} existe en la fila {}'
 
@@ -90,19 +89,19 @@ class pruebaApi(APIView):
         
         return None
 
-    def send_vertical(self, index, word, row, positions, word_search):
+    def search_vertical(self, word, row, positions, word_search):
 
         build_word = ''
         msg = 'Vertical: la palabra {} existe desde la fila {} hasta la fila {}, en la posicion {}.'
 
-        if (row + len(word)) <= 13:
+        if (row + len(word)) <= _MAX_ROW_LEN:
 
             for position in positions:
                 build_word = ''
 
                 for i, value in enumerate(word):
 
-                    if value is not word_search[i][position]:
+                    if value != word_search[i][position]:
                         break
 
                     build_word += value
@@ -110,14 +109,14 @@ class pruebaApi(APIView):
                 if word == build_word:
                     return msg.format(word, row + 1, row + len(word), position + 1)
 
-        if ((row + 1) - len(word) >= 0):
+        if ((row + 1) - len(word) >= _MIN_ROW_LEN):
             
             for position in positions:
                 build_word = ''
 
                 for i, value in enumerate(word):
 
-                    if value is not word_search[row - i][position]:
+                    if value != word_search[row - i][position]:
                         break
 
                     build_word += value
@@ -125,21 +124,21 @@ class pruebaApi(APIView):
                 if word == build_word:
                     return msg.format(word, row + 1, (row + 1) - len(word), position + 1)
 
-    def send_diagonal(self, index, word, row, positions, word_search):
+    def send_diagonal(self, word, row, positions, word_search):
         
         build_word = ''
         msg = 'Diagonal: La palabra {} inica en ({}, {}) y finaliza en ({}, {})'
 
-        if (row + len(word)) <= 13:
+        if (row + len(word)) <= _MAX_ROW_LEN:
 
             for position in positions:
                 build_word = ''
 
-                if (position + len(word)) <= 13:
+                if (position + len(word)) <= _MAX_ROW_LEN:
                     
                     for i, value in enumerate(word):
 
-                        if value is not word_search[row + i][position + i]:
+                        if value != word_search[row + i][position + i]:
                             break
 
                         build_word += value
@@ -147,11 +146,11 @@ class pruebaApi(APIView):
                     if word == build_word:
                         return msg.format(word, row + 1, position + 1, (row + 1) + len(word), (position + 1) + len(word))
 
-                if ((position + 1) - len(word)) >= 0:
+                if ((position + 1) - len(word)) >= _MIN_ROW_LEN:
 
                     for i, value in enumerate(word):
 
-                        if value is not word_search[row + i][position - i]:
+                        if value != word_search[row + i][position - i]:
                             break
 
                         build_word += value
@@ -159,15 +158,15 @@ class pruebaApi(APIView):
                     if word == build_word:
                         return msg.format(word, row + 1, position + 1, (row + 1) + len(word), (position + 1) - len(word))
 
-        if ((row + 1) - len(word) >= 0):
+        if ((row + 1) - len(word) >= _MIN_ROW_LEN):
 
             for position in positions:
 
-                if ((position + 1) - len(word)) >= 0:
+                if ((position + 1) - len(word)) >= _MIN_ROW_LEN:
 
                     for i, value in enumerate(word):
 
-                        if value is not word_search[row - i][position - i]:
+                        if value != word_search[row - i][position - i]:
                             break
 
                         build_word += value
@@ -175,10 +174,10 @@ class pruebaApi(APIView):
                     if word == build_word:
                         return msg.format(word, row + 1, position + 1, (row + 1) - len(word), (position + 1) - len(word))
 
-                if (position + len(word)) <= 13:
+                if (position + len(word)) <= _MAX_ROW_LEN:
                     for i, value in enumerate(word):
 
-                        if value is not word_search[row - i][position + i]:
+                        if value != word_search[row - i][position + i]:
                             break
 
                         build_word += value
